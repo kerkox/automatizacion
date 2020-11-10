cee9458a95bf7a0967649f6e29fc9420a2b2c6c5
Paul Cortes <paulkerg@gmail.com>
2020-11-10 12:05:40 -0500

    Movida de modulos a sidebarService

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -10,25 +10,5 @@ export const types = {
     OrdenesPedidos: "orden-pedido",
     Inventario: "inventario"
 
-  },
-  MODULOS: [
-    { 
-      name: 'ERP',
-      menus: [
-        { path: '/erp/inventario', name: 'Inventario' },
-        { path: '/erp/materia-prima', name: 'Materia Prima' },
-        { path: '/erp/orden-pedido', name: 'Orden Pedido' },
-        { path: '/erp/parametros-referencia', name: 'Parametros Referencia' }
-      ]
-    },
-    {
-      name: 'MES',
-      menus: [
-        { path: '/mes/orden-produccion', name: 'Orden produccion' },
-      ]
-    }
-    
-    
-  ]
-
+  },  
 }
