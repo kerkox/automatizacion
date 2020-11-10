dd831f2dec3dc2573f37950fefe08b854a2316ee
Paul Cortes <paulkerg@gmail.com>
2020-11-10 10:16:15 -0500

    mejora en orden de pedido

--- src/app/pages/erp/orden-pedido/orden-pedido.component.ts
+++ src/app/pages/erp/orden-pedido/orden-pedido.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { GeneralService } from './../../../services/general.service';
 import { OrdenPedido } from '../../../interfaces/orden-pedido.interface';
 import { Component, OnInit } from '@angular/core';
@@ -40,54 +40,33 @@ export class OrdenPedidoComponent implements OnInit {
     });
   }
 
+  isInvalid_campo(campo: AbstractControl) {
+    return campo.invalid && campo.touched
+  }
+  
   get cliente() {
     return this.formOrdenPedido.get('cliente')
-  }
-
-  get isInvalid_cliente() {
-    return this.cliente.invalid && this.cliente.touched
-  }
+  }  
 
   get cantidad() {
     return this.formOrdenPedido.get('cantidad')
-  }
-
-  get isInvalid_cantidad() {
-    return this.cantidad.invalid && this.cantidad.touched
   }
 
   get referencia_producto_id() {
     return this.formOrdenPedido.get('referencia_producto_id')
   }
 
-  get isInvalid_referencia_producto_id() {
-    return this.referencia_producto_id.invalid && this.referencia_producto_id.touched
-  }
-
   get tipo_producto_id() {
     return this.formOrdenPedido.get('tipo_producto_id')
-  }
-
-  get isInvalid_tipo_producto_id() {
-    return this.tipo_producto_id.invalid && this.tipo_producto_id.touched
   }
 
   get presentacion_producto_id() {
     return this.formOrdenPedido.get('presentacion_producto_id')
   }
 
-  get isInvalid_presentacion_producto_id() {
-    return this.presentacion_producto_id.invalid && this.presentacion_producto_id.touched
-  }
-
   get prioridad_id() {
     return this.formOrdenPedido.get('prioridad_id')
   }
-
-  get isInvalid_prioridad_id() {
-    return this.prioridad_id.invalid && this.prioridad_id.touched
-  }
-
 
   cargarDatos() {
     this.consultarOrdenesPedido();
